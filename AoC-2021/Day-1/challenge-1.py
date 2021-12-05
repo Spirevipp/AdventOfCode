be0909c95b144d335ea20a43715067497704b9bd
Spirevipp <bjornar.helgeland@hotmail.com>
2021-12-05 17:17:29 +0100

    Day 1 challenge 1 + 2

---
+++ AoC-2021/Day-1/challenge-1.py
@@ -0,0 +1,33 @@
+"""
+    AOC Day 1
+    https://adventofcode.com/2021/day/1
+"""
+import os
+
+print("Sonar Sweep depth measurement increase finder is now running")
+inputFilePath = f"{os.getcwd()}\\Aoc-2021\\Day-1\\Challenge-1\\input.txt"
+inputFile = open(inputFilePath, "rt", encoding="utf-8")
+
+# parse inputFile as a list of ints
+inputList = [int(x) for x in inputFile.read().splitlines()]
+
+compareVal = inputList[0]
+index = 1
+increased = 0
+while index < len(inputList):
+    currentNum = int(inputList[index])
+    # print(f"comparing {compareVal} to {currentNum}")
+
+    if currentNum == compareVal:
+        print(f"{currentNum} is equal to {compareVal}")
+        pass
+    elif currentNum > compareVal:
+        print(f"{currentNum} is bigger than {compareVal}")
+        increased += 1
+        pass
+    elif currentNum < compareVal:
+        print(f"{currentNum} is smaller than {compareVal}")
+        pass
+    compareVal = inputList[index]
+    index += 1
+print(f"Measurement increased {increased} times")
---
+++ AoC-2021/Day-1/challenge-2.py
@@ -0,0 +1,41 @@
+"""
+    AOC Day 1
+    https://adventofcode.com/2021/day/1
+"""
+import os
+
+print("Sonar Sweep depth measurement increase finder is now running")
+inputFilePath = f"{os.getcwd()}\\Aoc-2021\\Day-1\\input.txt"
+inputFile = open(inputFilePath, "rt", encoding="utf-8")
+
+# parse inputFile as a list of ints
+inputListTmp = [int(x) for x in inputFile.read().splitlines()]
+
+# combine 3 subsequent ints to form a new int and add to inputList
+inputList = []
+for i in range(0, len(inputListTmp)):
+    if i == len(inputListTmp) - 2:
+        break
+    inputList.append(inputListTmp[i] + inputListTmp[i + 1] + inputListTmp[i + 2])
+    i += 1
+
+compareVal = inputList[0]
+index = 1
+increased = 0
+while index < len(inputList):
+    currentNum = int(inputList[index])
+    # print(f"comparing {compareVal} to {currentNum}")
+
+    if currentNum == compareVal:
+        print(f"{currentNum} is equal to {compareVal}")
+        pass
+    elif currentNum > compareVal:
+        print(f"{currentNum} is bigger than {compareVal}")
+        increased += 1
+        pass
+    elif currentNum < compareVal:
+        print(f"{currentNum} is smaller than {compareVal}")
+        pass
+    compareVal = inputList[index]
+    index += 1
+print(f"Measurement increased {increased} times")
